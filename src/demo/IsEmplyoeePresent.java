06c341afd8be45d694aa694429d6b2f544cee205
priya <priyapandey8884.blr@gmail.com>
2022-06-30 18:17:10 +0530

    added

--- src/demo/IsEmplyoeePresent.java
+++ src/demo/IsEmplyoeePresent.java
@@ -4,38 +4,49 @@ public class IsEmplyoeePresent {
 
 			
 			
+	static final int WagePerHour = 20;
+	static final int FullDayHour = 8;
+	static final int PartTimeHour = 4;
+	static final int IsPartTime = 0;
+	static final int IsFulltime = 1;
+	static final int WorkingDayInMonth = 20;
+	static final int WorkingHourPerMonth = 100;
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage");
-		// TODO Auto-generated method stub
-		int employeePresent = 1;
-		int employeePartTime = 2;
-		int wagePerHour = 20;
-		int numberOfHours = 8;
-		int numberOfHoursForPartTime = 4;
-		int workingDayInAMonth = 20;
-		int isPresent = (int) Math.floor(Math.random() * 3);
-		switch (isPresent) {
-		case 0:
-			System.out.println("Employee is Present");
-			System.out.println("Employee earn = " + numberOfHours * wagePerHour);
-			System.out.println("Employee earn per Month =" + numberOfHours * wagePerHour * workingDayInAMonth);
-		break;
-		case 1:
-			System.out.println("Employee is doing Part Time");
-			System.out.println("Employee earn = " + numberOfHoursForPartTime * wagePerHour);
-			System.out.println("Employee earn per Month =" + numberOfHoursForPartTime * wagePerHour * workingDayInAMonth);
-
-		break;
-		case 2:
-			System.out.println("Employee is not Present");
-			System.out.println("Employee earn = 0");
-			System.out.println("Employee earn = 0");
-		break;
+		System.out.println("Welcome to Employee Wage Problem ");
+		int dailyWages = 0;
+		int MonthlyWages = 0;
+		int workingDay = 1;
+		int day = 1;
+		int totalWorkingHoursInMonth = 0;
+		while (workingDay <= WorkingDayInMonth && totalWorkingHoursInMonth <= WorkingHourPerMonth) {
+			int attendance = (int) Math.floor(Math.random() * 10) % 3;
+			switch (attendance) {
+			case IsPartTime:
+				System.out.println("Day " + day + " Half day");
+				dailyWages = WagePerHour * PartTimeHour;
+				System.out.println("Wages of the day " + day + " is " + dailyWages);
+				MonthlyWages = dailyWages + MonthlyWages;
+				totalWorkingHoursInMonth = totalWorkingHoursInMonth + PartTimeHour;
+				break;
+			case IsFulltime:
+				System.out.println("Day " + day + " present");
+				dailyWages = WagePerHour * FullDayHour;
+				System.out.println("Wages of the day " + day + " is " + dailyWages);
+				MonthlyWages = dailyWages + MonthlyWages;
+				totalWorkingHoursInMonth = totalWorkingHoursInMonth + FullDayHour;
+				break;
+			default:
+				System.out.println("Day " + day + " Absent");
+				break;
+			}
+			day++;
+			workingDay++;
 		}
+		System.out.println("Total Working Hours of the month: " + totalWorkingHoursInMonth);
+		System.out.println("Wages of the month: " + MonthlyWages);
 	}
-}
-
-		
+}	
 	
 	
 	
