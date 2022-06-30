2bad7ed6bbcddcf1df3d19951e8890890bb28df6
priya <priyapandey8884.blr@gmail.com>
2022-06-30 17:51:54 +0530

    Added:Using Switch

--- src/demo/IsEmplyoeePresent.java
+++ src/demo/IsEmplyoeePresent.java
@@ -3,31 +3,30 @@ package demo;
 public class IsEmplyoeePresent {
 
 			
-			// TODO Auto-generated method stub
+			
 	public static void main(String[] args) {
-		System.out.println("welcome to emplyoee wage");
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage");
+		
 		int employeePresent = 1;
+		int employeePartTime = 2;
 		int wagePerHour = 20;
 		int numberOfHours = 8;
-		int partTime = 4;
-		int isPresent =(int) Math.floor(Math.random() * 10 )%3;
-		if (isPresent == 0) {
-			System.out.println("emplyoee is absent");
-			System.out.println("emplyoee earn = 0");
-			}
-		else if (isPresent == 1){
-				System.out.println(" Emplyoee is present");
-				System.out.println("emplyoee earn =" +numberOfHours*wagePerHour);			
-				  }
-		else 
-		{
-			
-			System.out.println("emplyoee working as a part time");
-		    System.out.println("emplyoee earn=" + partTime*numberOfHours);
+		int numberOfHoursForPartTime = 4;
+		int isPresent = (int) Math.floor(Math.random() * 3);
+		switch (isPresent) {
+		case 0:
+			System.out.println("Employee is Present");
+			System.out.println("Employee earn = " + numberOfHours * wagePerHour);
+		break;
+		case 1:
+			System.out.println("Employee is doing Part Time");
+			System.out.println("Employee earn = " + numberOfHoursForPartTime * wagePerHour);
+		break;
+		case 2:
+			System.out.println("Employee is not Present");
+			System.out.println("Employee earn = 0");
+		break;
 		}
 	}
-	
-
 }
 
