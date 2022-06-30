4c0b22d974fa29a765ed2f8b912a19e2eedbc5ab
priya <priyapandey8884.blr@gmail.com>
2022-06-30 17:47:03 +0530

    Added UC2

--- src/demo/IsEmplyoeePresent.java
+++ src/demo/IsEmplyoeePresent.java
@@ -2,20 +2,24 @@ package demo;
 
 public class IsEmplyoeePresent {
 
-		public static void main(String[] args) {
-			System.out.println("welcome to emplyoee wage");
+			
 			// TODO Auto-generated method stub
-			int employeePresent = 1;
-			int wagePerHour = 20;
-			int numberOfHours = 8; 
-			int isPresent =(int) Math.floor(Math.random() * 10 )%2;
-			if (isPresent == employeePresent)
-			
-			
-				System.out.println("emplyoee is presennt");
-				else
-					System.out.println("not present");
+			public static void main(String[] args) {
+				System.out.println("welcome to emplyoee wage");
+				// TODO Auto-generated method stub
+				int employeePresent = 1;
+				int wagePerHour = 20;
+				int numberOfHours = 8; 
+				int isPresent =(int) Math.floor(Math.random() * 10 )%2;
+				if (isPresent == employeePresent) {
+					System.out.println("emplyoee is presennt");
+					System.out.println("emplyoee earn =" +numberOfHours*wagePerHour);
+					}else {
+						System.out.println("not present");
+						System.out.println("emplyoee earn = 0");
 
 	}
-
+			}
 }
+
+
