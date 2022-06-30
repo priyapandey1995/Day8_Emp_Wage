384edac2777976ff01086a7d29dd73d30886c4e6
priya <priyapandey8884.blr@gmail.com>
2022-06-30 17:49:16 +0530

    Added part-time wage

--- src/demo/IsEmplyoeePresent.java
+++ src/demo/IsEmplyoeePresent.java
@@ -4,22 +4,30 @@ public class IsEmplyoeePresent {
 
 			
 			// TODO Auto-generated method stub
-			public static void main(String[] args) {
-				System.out.println("welcome to emplyoee wage");
-				// TODO Auto-generated method stub
-				int employeePresent = 1;
-				int wagePerHour = 20;
-				int numberOfHours = 8; 
-				int isPresent =(int) Math.floor(Math.random() * 10 )%2;
-				if (isPresent == employeePresent) {
-					System.out.println("emplyoee is presennt");
-					System.out.println("emplyoee earn =" +numberOfHours*wagePerHour);
-					}else {
-						System.out.println("not present");
-						System.out.println("emplyoee earn = 0");
-
-	}
+	public static void main(String[] args) {
+		System.out.println("welcome to emplyoee wage");
+		// TODO Auto-generated method stub
+		int employeePresent = 1;
+		int wagePerHour = 20;
+		int numberOfHours = 8;
+		int partTime = 4;
+		int isPresent =(int) Math.floor(Math.random() * 10 )%3;
+		if (isPresent == 0) {
+			System.out.println("emplyoee is absent");
+			System.out.println("emplyoee earn = 0");
 			}
-}
+		else if (isPresent == 1){
+				System.out.println(" Emplyoee is present");
+				System.out.println("emplyoee earn =" +numberOfHours*wagePerHour);			
+				  }
+		else 
+		{
+			
+			System.out.println("emplyoee working as a part time");
+		    System.out.println("emplyoee earn=" + partTime*numberOfHours);
+		}
+	}
+	
 
+}
 
